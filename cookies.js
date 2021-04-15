ff88215237be3436322c46b58e9eba434de2983f
houseplayer <houseplayer@gmail.com>
2021-04-16 00:11:51 +0200

    Cookies functionality improved

--- cookies.js
+++ cookies.js
@@ -1,20 +1,21 @@
+import { getData } from './main.js';
+
 const createExpiryDateForCookie = () => {
-	const cookieExpiryDate = new Date()
-	const expireDay = cookieExpiryDate.getDate()
-	cookieExpiryDate.setDate(expireDay + 1)
-	return cookieExpiryDate
-}
+	const cookieExpiryDate = new Date();
+	const expireDay = cookieExpiryDate.getDate();
+	cookieExpiryDate.setDate(expireDay + 1);
+	return cookieExpiryDate;
+};
 
 export const setCookie = async () => {
-	const res = await fetch("https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json")
-	const data = await res.json()
-	let cookieValue = ""
-	const vendors = [...Object.values(data.vendors)]
-	vendors.forEach(vendor => {
-		cookieValue += `${vendor.name.trim()};`
-	})
-
-	const cookieExpiryDate = createExpiryDateForCookie()
-	console.log(cookieValue)
-	// document.cookie = `accepted=${cookieValue}; expires=${cookieExpiryDate}`
-}
+	let cookieValue = '';
+	getData()
+		.then(vendors => {
+			vendors.forEach(vendor => {
+				cookieValue += `${vendor.name.trim()}-`;
+			});
+			const cookieExpiryDate = createExpiryDateForCookie();
+			document.cookie = `accepted=${cookieValue};expires=${cookieExpiryDate};secure=true`;
+		})
+		.catch(error => console.error(error));
+};
--- main.js
+++ main.js
@@ -1,35 +1,52 @@
-import { setCookie } from "./cookies.js"
+import { setCookie } from './cookies.js';
 
-const mainContainer = document.querySelector(".main_container")
-const cookiesPolicyContainer = document.querySelector(".cookies_policy_container")
-const vendorsList = document.querySelector(".GDPR_vendors_list")
-const GDPRAcceptBtn = document.querySelector(".GDPR_accept_btn")
-const GDPRRejectBtn = document.querySelector(".GDPR_reject_btn")
+const mainContainer = document.querySelector('.main_container');
+const cookiesPolicyContainer = document.querySelector('.cookies_policy_container');
+const vendorsList = document.querySelector('.GDPR_vendors_list');
+const GDPRAcceptBtn = document.querySelector('.GDPR_accept_btn');
+const GDPRRejectBtn = document.querySelector('.GDPR_reject_btn');
 
-setCookie()
+export const getData = () => {
+	return fetch('https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json')
+		.then(res => res.json())
+		.then(data => {
+			const vendors = [...Object.values(data.vendors)];
+			return vendors;
+		});
+};
 
-const getData = async () => {
-	const res = await fetch("https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json")
-	const data = await res.json()
-	const vendors = [...Object.values(data.vendors)]
+const createVendorsList = vendors => {
 	vendors.forEach(vendor => {
-		const listElement = document.createElement("li")
+		const listElement = document.createElement('li');
 		listElement.innerHTML = `
-        <input type="checkbox" checked id="${vendor.name}" /><label for="${vendor.name}">${vendor.name}</label>
-        <h5><a href="${vendor.policyUrl}">${vendor.policyUrl}</a></h5>`
-		vendorsList.append(listElement)
-		cookiesPolicyContainer.style.display = "block"
-	})
-}
+		<input type="checkbox" checked id="${vendor.name}" />
+		<label for="${vendor.name}">${vendor.name}</label>
+		<h5><a href="${vendor.policyUrl}">${vendor.policyUrl}</a></h5>
+		`;
+		vendorsList.append(listElement);
+		cookiesPolicyContainer.style.display = 'block';
+	});
+};
 
-getData()
+const init = () => {
+	if (document.cookie.indexOf('accepted=') === -1) {
+		mainContainer.classList.add('modal_open');
+		getData()
+			.then(vendors => createVendorsList(vendors))
+			.catch(error => console.error(error));
+	}
+};
 
-GDPRAcceptBtn.addEventListener("click", () => {
-	mainContainer.classList.remove("modal_open")
-	cookiesPolicyContainer.style.display = "none"
-})
+const closeModal = () => {
+	mainContainer.classList.remove('modal_open');
+	cookiesPolicyContainer.style.display = 'none';
+};
 
-GDPRRejectBtn.addEventListener("click", () => {
-	mainContainer.classList.remove("modal_open")
-	cookiesPolicyContainer.style.display = "none"
-})
+GDPRAcceptBtn.addEventListener('click', () => {
+	setCookie();
+	closeModal();
+});
+
+GDPRRejectBtn.addEventListener('click', closeModal);
+
+init();
